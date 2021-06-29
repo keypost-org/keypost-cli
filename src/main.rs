2f711ee4f12a1c97234bf4b88ca3e9c1a2455744
Jeff Rade <jeffrade@gmail.com>
2021-06-28 19:52:22 -0500

    Add a Server struct to further decouple the client-server communication

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::io::{Error, ErrorKind};
 use std::process::exit;
 
 use curve25519_dalek::ristretto::RistrettoPoint;
-use opaque_ke::keypair::KeyPair;
+use opaque_ke::keypair::{Key, KeyPair};
 use opaque_ke::ClientRegistrationStartResult;
 use opaque_ke::{
     ciphersuite::CipherSuite, rand::rngs::OsRng, ClientRegistration,
@@ -14,6 +14,12 @@ use opaque_ke::{
     ClientLogin, ClientLoginFinishParameters, ClientLoginStartParameters, CredentialFinalization,
     CredentialRequest, CredentialResponse, ServerLogin, ServerLoginStartParameters,
 };
+
+struct Server {
+    pub public_key: Key,
+    pub registration_state: Vec<u8>,
+    pub response: Vec<u8>,
+}
 
 // The ciphersuite trait allows to specify the underlying primitives
 // that will be used in the OPAQUE protocol
@@ -52,37 +58,47 @@ fn client_side_registration_finish(
     base64::encode(client_message_bytes)
 }
 
-fn server_side_registration_start(
-    registration_request_base64: &str,
-    server_kp: &KeyPair<RistrettoPoint>,
-) -> ServerRegistrationStartResult<Default> {
+fn server_side_registration_start(registration_request_base64: &str, mut server: Server) -> Server {
     let registration_request_bytes =
         base64::decode(registration_request_base64).expect("Could not perform base64 decode");
-    let server_public_key = server_kp.public();
+    let server_public_key = server.public_key.clone();
     let mut server_rng = OsRng;
-    ServerRegistration::<Default>::start(
-        &mut server_rng,
-        RegistrationRequest::deserialize(&registration_request_bytes[..]).unwrap(),
-        server_public_key,
-    )
-    .unwrap()
+    let server_registration_start_result: ServerRegistrationStartResult<Default> =
+        ServerRegistration::<Default>::start(
+            &mut server_rng,
+            RegistrationRequest::deserialize(&registration_request_bytes[..]).unwrap(),
+            &server_public_key,
+        )
+        .unwrap();
+    let registration_response_bytes = server_registration_start_result.message.serialize();
+    let server_registration_bytes = server_registration_start_result.state.serialize();
+    assert_eq!(
+        &server_registration_bytes,
+        &ServerRegistration::<Default>::deserialize(&server_registration_bytes)
+            .unwrap()
+            .serialize()
+    );
+    server.registration_state = server_registration_bytes;
+    server.response = registration_response_bytes;
+    server
 }
 
-fn server_side_registration_finish(
-    client_message_base64: String,
-    server_registration_bytes: &[u8],
-) -> Vec<u8> {
+fn server_side_registration_finish(client_message_base64: String, server: Server) -> Vec<u8> {
+    let server_registration_bytes = server.registration_state;
     let client_message_bytes =
         base64::decode(client_message_base64).expect("Could not perform base64 decode");
     let server_registration =
-        ServerRegistration::<Default>::deserialize(server_registration_bytes).unwrap();
+        ServerRegistration::<Default>::deserialize(&server_registration_bytes).unwrap();
     let password_file = server_registration
         .finish(RegistrationUpload::deserialize(&client_message_bytes[..]).unwrap())
         .unwrap();
     password_file.serialize()
 }
 
-fn account_registration(client_password: String, server_kp: &KeyPair<RistrettoPoint>) -> Vec<u8> {
+// https://docs.rs/opaque-ke/0.5.0/opaque_ke/#structs
+// https://docs.rs/opaque-ke/0.5.0/opaque_ke/struct.ServerRegistrationStartResult.html
+// https://docs.rs/opaque-ke/0.5.0/opaque_ke/struct.ServerRegistration.html
+fn account_registration(client_password: String, server: Server) -> Vec<u8> {
     let mut client_rng = OsRng;
     let client_registration_start_result =
         client_side_registration(&mut client_rng, client_password);
@@ -94,23 +110,16 @@ fn account_registration(client_password: String, server_kp: &KeyPair<RistrettoPo
             .replace("{{__DATA__}}", &registration_request_base64)
             .replace("{{__URLPATH__}}", "register/start")
     );
-    let server_registration_start_result =
-        server_side_registration_start(&registration_request_base64, &server_kp);
-    let registration_response_bytes = server_registration_start_result.message.serialize();
-    let server_registration_bytes = server_registration_start_result.state.serialize();
-    assert_eq!(
-        &server_registration_bytes,
-        &ServerRegistration::<Default>::deserialize(&server_registration_bytes)
-            .unwrap()
-            .serialize()
-    );
-    let registration_response_base64 = base64::encode(&registration_response_bytes);
+
+    let server = server_side_registration_start(&registration_request_base64, server);
+    let registration_response_bytes = server.response.as_slice();
+    let registration_response_base64 = base64::encode(registration_response_bytes);
     let client_message_base64 = client_side_registration_finish(
         &mut client_rng,
         client_registration_start_result,
         &registration_response_base64,
     );
-    server_side_registration_finish(client_message_base64, &server_registration_bytes)
+    server_side_registration_finish(client_message_base64, server)
     // the password_file
 }
 
@@ -121,14 +130,14 @@ curl -X POST --header "Content-Type: application/json" --data '{"id": {{__ID__}}
 
 fn main() {
     let mut server_rng = OsRng;
-    let server_kp = Default::generate_random_keypair(&mut server_rng);
+    let server_kp: KeyPair<RistrettoPoint> = Default::generate_random_keypair(&mut server_rng);
+    let mut registered_users = HashMap::<String, Vec<u8>>::new();
 
     let mut rl = rustyline::Editor::<()>::new();
-    let mut registered_users = HashMap::<String, Vec<u8>>::new();
     loop {
         println!(
             "\nCurrently registered usernames: {:?}\n",
-            registered_users.keys()
+            &registered_users.keys()
         );
 
         println!("Enter an option (1 or 2):");
@@ -145,7 +154,12 @@ fn main() {
                 let password = get_string("Password", &mut rl, true);
                 match line.as_ref() {
                     "1" => {
-                        let password_file_bytes = account_registration(password, &server_kp);
+                        let server = Server {
+                            public_key: server_kp.public().clone(),
+                            registration_state: vec![],
+                            response: vec![],
+                        };
+                        let password_file_bytes = account_registration(password, server);
                         let password_file_base64: String = base64::encode(&password_file_bytes);
                         println!(
                             "{}",
